d5e59f5ecef5c858863c604c6c16f268611ba2f4
Nikita <nikita.arhipov4@gmail.com>
2022-03-20 15:43:59 +0700

    Изменил переключение дочерних чекбоксов

--- Assets/Scripts/RelationWithSubToggleScript.cs
+++ Assets/Scripts/RelationWithSubToggleScript.cs
@@ -6,38 +6,27 @@ using UnityEngine.UI;
 public class RelationWithSubToggleScript : MonoBehaviour
 {
     [SerializeField] private Toggle[] toggles;
-    [SerializeField] private GameObject Content;
     [SerializeField] private Toggle Parent;
-
-
-    private int amount = 0;
 
     void Start()
     {
-        amount = Content.transform.childCount - 1;
 
-        toggles = new Toggle[amount];
-
-        for (int i = 0; i < amount; i++)
-        {
-            toggles[i] = Content.transform.GetChild(i).GetComponent<Toggle>();
-        }
     }
 
     public void ChangeChildren()
     {
         if (Parent.isOn)
         {
-            for (int i = 0; i < amount; i++)
+            foreach(Toggle tog in toggles)
             {
-                toggles[i].isOn = true;
+                tog.isOn = true;
             }
         }
         else
         {
-            for (int i = 0; i < amount; i++)
+            foreach (Toggle tog in toggles)
             {
-                toggles[i].isOn = false;
+                tog.isOn = false;
             }
         }
     }
